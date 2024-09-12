d696f750950b91ab55378a7881470f2a3eee362d
Jean Nunes <jean.nunes@uzzipay.com>
2024-09-12 01:14:58 -0400

    Add Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,11 +7,7 @@ export default function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/jaon82.png"
-            alt=""
-          />
+          <Avatar src="https://github.com/jaon82.png" alt="" />
           <div className={styles.authorInfo}>
             <strong>Jean Nunes</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 
 export default function Sidebar() {
@@ -10,11 +11,7 @@ export default function Sidebar() {
         alt=""
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/jaon82.png"
-          alt=""
-        />
+        <Avatar src="https://github.com/jaon82.png" alt="" />
         <strong>Jean Nunes</strong>
         <span>Web Developer</span>
       </div>
